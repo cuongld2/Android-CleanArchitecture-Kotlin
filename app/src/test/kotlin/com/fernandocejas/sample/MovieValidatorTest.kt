12ca63e8cd902cc0a8be879d652746e91af376fb
Donald Le <donald.le@iamondemand.com>
2022-03-31 13:42:55 +0700

    Update jUnit assert and move unit test into one test only

--- app/src/test/kotlin/com/fernandocejas/sample/MovieValidatorTest.kt
+++ app/src/test/kotlin/com/fernandocejas/sample/MovieValidatorTest.kt
@@ -1,20 +1,18 @@
 package com.fernandocejas.sample
 
 import com.fernandocejas.sample.core.functional.MovieValidator
-import junit.framework.Assert.assertFalse
-import junit.framework.Assert.assertTrue
+import org.junit.Assert.assertTrue
+import org.junit.Assert.assertFalse
 import org.junit.Test
 
 
 class MovieValidatorTest {
 
     @Test
-    fun movieValidator_CorrectCase() {
+    fun isValidMovie() {
         assertTrue(MovieValidator.isValidMovie("The lord of the rings"))
-    }
-
-    @Test fun movieValidator_FailCase() {
         assertFalse(MovieValidator.isValidMovie("name@email"))
+
     }
 
 }
