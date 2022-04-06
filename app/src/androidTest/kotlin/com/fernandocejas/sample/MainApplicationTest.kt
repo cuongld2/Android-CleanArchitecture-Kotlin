277e5232cc55ba1f37f59f2c3853f69330251fee
Donald Le <donald.le@iamondemand.com>
2022-04-07 00:16:29 +0700

    Add assert for instrumentation test

--- app/src/androidTest/kotlin/com/fernandocejas/sample/MainApplicationTest.kt
+++ app/src/androidTest/kotlin/com/fernandocejas/sample/MainApplicationTest.kt
@@ -8,11 +8,15 @@ import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 
 import androidx.test.rule.ActivityTestRule
 import com.fernandocejas.sample.features.movies.MoviesActivity
+import junit.framework.Assert.assertTrue
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -39,7 +43,8 @@ class MainApplicationTest {
 
         val movieListButton = onView(withId(R.id.movieList))
         movieListButton.perform(click())
-
+        val moviePoster = onView(withId(R.id.moviePoster))
+        moviePoster.check(matches(isDisplayed()))
     }
 
 
